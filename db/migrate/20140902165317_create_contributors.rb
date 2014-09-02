fd31c7d28796b47d0f2fed741ce56402bff8d41d
Carolyn Huynh & Scott Dudley <student@epicodus.com>
2014-09-02 10:04:55 -0700

    Create contributor class

---
+++ db/migrate/20140902165317_create_contributors.rb
@@ -0,0 +1,8 @@
+class CreateContributors < ActiveRecord::Migration
+  def change
+    create_table :contributors do |t|
+      t.string :name
+      t.timestamps
+    end
+  end
+end
---
+++ spec/contributor_spec.rb
@@ -0,0 +1,6 @@
+require 'rails_helper'
+
+describe Contributor do
+  it { should validate_presence_of :name }
+
+end
