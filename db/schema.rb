1f6b69932657a9f19ad5fc22e7c402465384e153
Carolyn Huynh & Scott Dudley <student@epicodus.com>
2014-09-02 10:26:29 -0700

    Create Tag table and class

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140902171742) do
+ActiveRecord::Schema.define(version: 20140902172328) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,12 @@ ActiveRecord::Schema.define(version: 20140902171742) do
     t.string   "name"
     t.string   "steps"
     t.integer  "contributor_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "tags", force: true do |t|
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
